07d8b8e4ff28aabf0c02ffa4b0cd81d6f9fd13d0
theeKirat1984 <amazingSevenfix@gmail.com>
2025-10-08 04:17:12 +0500

    quickfix

---
+++ laravel/database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Domain\User\Entities\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // 
+    }
+}
