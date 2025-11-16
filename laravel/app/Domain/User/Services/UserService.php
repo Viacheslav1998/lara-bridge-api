b12dc09202002d1935356f6e462c145e3c0ec517
Slavik <amazingSevenfix@gmail.com>
2025-11-17 01:22:33 +0500

    qf

--- laravel/app/Domain/User/Services/UserService.php
+++ laravel/app/Domain/User/Services/UserService.php
@@ -5,44 +5,45 @@ namespace App\Domain\User\Services;
 use App\Domain\User\Repositories\UserRepository;
 use App\Exceptions\InvalidFilterException;
 
-class UserService 
-{ 
-	private $allowedFilters = ['country', 'first_name', 'email'];
+class UserService
+{
+    private $allowedFilters = ['country', 'first_name', 'email'];
 
-	public function __construct(
-		protected UserRepository $repository
-	) {}
+    public function __construct(
+        protected UserRepository $repository
+    ) {
+    }
 
-	public function findUsersByFilters(array $filters)
-	{
-			$InvalidFilters = array_diff(array_keys($filters), $this->allowedFilters);
+    public function findUsersByFilters(array $filters)
+    {
+        $InvalidFilters = array_diff(array_keys($filters), $this->allowedFilters);
 
-		if (!empty($InvalidFilters)) {
-			$InvalidFilterName = reset($InvalidFilters);
-				throw new InvalidFilterException("Invaid filter: '{$InvalidFilterName}' . Allowed filters are: " . implode(', ', $this->allowedFilters));
-		}
+        if (!empty($InvalidFilters)) {
+            $InvalidFilterName = reset($InvalidFilters);
+            throw new InvalidFilterException("Invaid filter: '{$InvalidFilterName}' . Allowed filters are: " . implode(', ', $this->allowedFilters));
+        }
 
-		return $this->repository->findByFilters($filters);
-	}
+        return $this->repository->findByFilters($filters);
+    }
 
-	public function getUsersCount()
-	{
-			return $this->repository->count();
-	}
-		public function sssss()
-	{
-			return $this->repository->count();
-	}
+    public function getUsersCount()
+    {
+        return $this->repository->count();
+    }
+    public function sssss()
+    {
+        return $this->repository->count();
+    }
 
 
-	public function getCurrentUser($id)
-	{
-		return $this->repository->find($id);
-	}
+    public function getCurrentUser($id)
+    {
+        return $this->repository->find($id);
+    }
 
-	public function createUser(array $data)
-	{
-		return $this->repository->create($data);
-	}
+    public function createUser(array $data)
+    {
+        return $this->repository->create($data);
+    }
 
-}   
+}
