67bcbefcf63c1e28a031dc05f98b8d1b3a7df2ed
Slavik <amazingSevenfix@gmail.com>
2025-11-30 18:04:19 +0500

    qf

--- laravel/app/Http/Controllers/Api/UserController.php
+++ laravel/app/Http/Controllers/Api/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use App\Actions\User\CreateUserAction;
 use App\Actions\User\FilterUserAction;
 use App\Actions\User\UpdateUserAction;
@@ -12,7 +13,7 @@ use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Http\Responses\ApiResponse;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class UserController
 {
