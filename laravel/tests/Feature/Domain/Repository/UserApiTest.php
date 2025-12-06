2ee0726e4083d9a69b385fb5adbbc6b9545bc0b3
Slavik <amazingSevenfix@gmail.com>
2025-12-06 22:58:01 +0500

    feat: upd user featch tests

--- laravel/tests/Feature/Domain/Repository/UserApiTest.php
+++ laravel/tests/Feature/Domain/Repository/UserApiTest.php
@@ -15,9 +15,13 @@ class UserApiTest extends TestCase
     {
         $userData = [
             'first_name' => $this->faker->firstName,
-            'email' => $this->faker->unique()->safeEmail,
-            'password' => 'password123',
-            'country' => 'USA'
+            'last_name' => $this->faker->lastName,
+            'country' => $this->faker->country(),
+            'phone' => $this->faker->phoneNumber(),
+            'number' => $this->faker->numberBetween(1,100),
+            'super' => $this->faker->boolean(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'bio' => $this->faker->paragraph(nb_senteces=3),
         ];
 
         $response = $this->postJson('/api/users', $userData);
