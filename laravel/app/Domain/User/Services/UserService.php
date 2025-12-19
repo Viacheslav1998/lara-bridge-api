1608f46e28f1fca79f173798edcd4503d82c2962
Slavik <amazingSevenfix@gmail.com>
2025-12-19 23:34:57 +0500

    qf

--- laravel/app/Domain/User/Services/UserService.php
+++ laravel/app/Domain/User/Services/UserService.php
@@ -4,7 +4,6 @@ namespace App\Domain\User\Services;
 
 use App\Domain\User\Repositories\UserRepository;
 use App\Exceptions\InvalidFilterException;
-use App\Http\Resources\UserResource;
 
 class UserService
 {
