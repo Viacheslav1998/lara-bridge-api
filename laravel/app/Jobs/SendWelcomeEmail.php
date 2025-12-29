eed6a3af433fc0e65879c6306af75d8cd5b739ab
Slavik <amazingSevenfix@gmail.com>
2025-12-30 04:19:10 +0500

    qf

--- laravel/app/Jobs/SendWelcomeEmail.php
+++ laravel/app/Jobs/SendWelcomeEmail.php
@@ -6,9 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Mail;
-
 
 class SendWelcomeEmail implements ShouldQueue
 {
