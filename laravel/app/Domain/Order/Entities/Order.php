836cb81ef931448c4b377c7474952dab80f8f2c4
Slavik <amazingSevenfix@gmail.com>
2026-01-31 22:13:55 +0500

    feat: Order

---
+++ laravel/app/Domain/Order/Entities/Order.php
@@ -0,0 +1,37 @@
+.<?php
+
+namespace App\Domain\Order\Entities;
+
+use Database\Factories\OrderFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'orders';
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = [
+     
+    ];
+
+    /**
+     * use Custom way factory
+     */
+    protected static function newFactory()
+    {
+        return OrderFactory::new();
+    }
+
+}
