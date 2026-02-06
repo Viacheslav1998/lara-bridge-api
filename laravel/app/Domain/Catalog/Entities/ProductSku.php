e590e7dd074a39848cadc1471b5f3ecfc433315c
Slavik <amazingSevenfix@gmail.com>
2026-02-06 16:55:34 +0500

    feat: productsku, fabric

---
+++ laravel/app/Domain/Catalog/Entities/ProductSku.php
@@ -0,0 +1,42 @@
+.<?php
+
+namespace App\Domain\Catalog\Entities;
+
+use Database\Factories\ProductFactory;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductSku extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'product_skus';
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array<int, string>
+    */
+    protected $fillable = [
+       'product_id',
+       'color',
+       'stock_count',
+       'size',
+       'price'
+    ];
+
+
+    /**
+     * use Custom way factory
+     */
+    protected static function newFactory()
+    {
+        return ProductSkuFactory::new();
+    }
+
+}
