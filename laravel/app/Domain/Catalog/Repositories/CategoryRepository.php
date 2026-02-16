052880a1632c4a638dec9a80f184fc174c17ef29
Slavik <amazingSevenfix@gmail.com>
2026-02-16 18:03:03 +0500

    feat: categoryRepository

---
+++ laravel/app/Domain/Catalog/Repositories/CategoryRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domain\Catalog\Repositories;
+
+use App\Domain\Catalog\Entities\Category;
+
+class CategoryRepository
+{
+    public function create(array $data): Category
+    {
+        return Category::create($data);
+    }
+}
