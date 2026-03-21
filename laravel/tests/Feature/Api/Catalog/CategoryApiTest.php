93a60b29712b48fe234d73adbca5d6948f20d024
Slavik <amazingSevenfix@gmail.com>
2026-03-21 14:36:17 +0500

    feat: test feature api, test work

--- laravel/tests/Feature/Api/Catalog/CategoryApiTest.php
+++ laravel/tests/Feature/Api/Catalog/CategoryApiTest.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace Tests\Feature\Api\Catalog;
+
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+
+class CategoryApiTest extends TestCase
+{
+    #[Test]
+    public function it_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
