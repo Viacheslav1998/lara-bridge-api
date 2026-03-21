13d2e1a16e5732ea6df9c4a9d16b39d26cd9763e
Slavik <amazingSevenfix@gmail.com>
2026-03-21 21:32:58 +0500

    feat: feature test

--- laravel/tests/Feature/Api/Catalog/CategoryApiTest.php
+++ laravel/tests/Feature/Api/Catalog/CategoryApiTest.php
@@ -2,16 +2,43 @@
 
 namespace Tests\Feature\Api\Catalog;
 
+use App\Domain\Catalog\Entities\Category;
 use PHPUnit\Framework\Attributes\Test;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CategoryApiTest extends TestCase
 {
-    #[Test]
-    public function it_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    #[Test]
+    public function test_can_create_category()
+    {
+        // 1. data
+        $payload = [
+            'name' => 'Phones',
+            'slug' => 'phones',
+        ];
+
+        // 2. request
+        $response = $this->postJson('/api/categories', $payload);
+
+        // 3. check answer
+        $response->assertStatus(201);
+
+        // 4. check json
+        $response->assertJson(fn ($json) => 
+          $json
+              ->whereType('name', 'string') 
+              ->whereType('slug', 'string')
+        );
+
+        // 5. check database
+        $this->assertDatabaseHas('categories', [
+            'name' => 'Phones',
+            'slug' => 'phones'
+        ]);
     }
+
+    
 }
