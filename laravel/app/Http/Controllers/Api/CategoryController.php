f379a2c041686dbb250f5a6f2376d5ff20122f17
Slavik <amazingSevenfix@gmail.com>
2026-03-25 22:33:15 +0500

    quickfix

--- laravel/app/Http/Controllers/Api/CategoryController.php
+++ laravel/app/Http/Controllers/Api/CategoryController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\Category\CategoryRequest;
-
 class CategoryController
 {
     public function test(): void
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\Analytics\UserAnalyticsController;
-use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test-user-job', [UserController::class, 'jobLog']);
