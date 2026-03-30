4a10647341ff25c040e2d2975cd30220c56aa755
Slavik <amazingSevenfix@gmail.com>
2026-03-30 21:53:54 +0500

    quickfix

--- laravel/app/Http/Controllers/Api/CategoryController.php
+++ laravel/app/Http/Controllers/Api/CategoryController.php
@@ -2,18 +2,14 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Actions\Category\CreateCategoryAction;
-use App\Http\Requests\Category\CreateCategoryRequest;
-use App\Http\Responses\ApiResponse;
-
 class CategoryController
 {
-   /**
-    * Store a Categories
-    */
-   public function store()
-   {
-      return 123;
-   }
+    /**
+     * Store a Categories
+     */
+    public function store()
+    {
+        return 123;
+    }
 
 }
