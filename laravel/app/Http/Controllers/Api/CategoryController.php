907137f97c7fb1005177399fd1146cb28e5bae26
Slavik <amazingSevenfix@gmail.com>
2026-04-03 18:53:18 +0500

    feat: category work

--- laravel/app/Http/Controllers/Api/CategoryController.php
+++ laravel/app/Http/Controllers/Api/CategoryController.php
@@ -2,14 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\CreateCategoryAction;
+use App\Http\Requests\Category\CreateCategoryRequest;
+// use App\Http\Requests\User\UpdateUserRequest;
+use App\Http\Responses\ApiResponse;
+
 class CategoryController
 {
-    /**
+
+   /**
      * Store a Categories
      */
-    public function store()
-    {
-        return 123;
-    }
+   public function store(CreateCategoryRequest $request, CreateCategoryAction $action)
+   {
+        $category = $action->execute($request->validated());
+
+        return ApiResponse::success()
+   }
 
 }
