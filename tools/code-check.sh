431b1d0c9f00a63160787252de0a5abf5188efb1
Slavik <amazingSevenfix@gmail.com>
2026-06-10 04:02:19 +0500

    quickfix

--- tools/code-check.sh
+++ tools/code-check.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-# define root
 ROOT_DIR=$(git rev-parse --show-toplevel)
 LARAVEL_DIR="$ROOT_DIR/laravel"
 
-# check PHP-CS-Fixer (dry-run)
 echo "-------------------------------------"
-echo "Checking PHP code style (dry-run)..."
+echo "Auto-fixing PHP code style..."
 echo "-------------------------------------"
 
-# --dry-run shows files that need to be fixed
+
 php "$LARAVEL_DIR/vendor/bin/php-cs-fixer" fix \
     --config="$LARAVEL_DIR/php-cs-fixer.dist.php" \
-    --dry-run --diff
+    --quiet
 
-echo ""
-echo "-------------------------------------"
-echo "Fixing PHP code style..."
-echo "-------------------------------------"
 
-# Fix it right now!
-php "$LARAVEL_DIR/vendor/bin/php-cs-fixer" fix \
-    --config="$LARAVEL_DIR/php-cs-fixer.dist.php"
+git add .
 
-echo ""
-echo "-------------------------------------"
-echo "PHP-CS-Fixer completed"
-echo "-------------------------------------"
+echo "Done! Code is clean."
